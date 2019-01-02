ddae177cf466f545d10b42c9c4ab237a5674aa91
dvskhamele <dvskha@gmail.com>
2019-01-03 00:24:41 +0530

    sk

--- teleadd-prateek/telapp/views.py
+++ teleadd-prateek/telapp/views.py
@@ -12,26 +12,77 @@ from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChannelAdminRights
 from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon.tl.functions.channels import InviteToChannelRequest
+import time
+
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
+
+#AMit
 api_id = 627710
 api_hash = '0a6cb002fc0c62fe84afe84932c3d1db'
-client = TelegramClient('session_name', api_id, api_hash).start()
-me=client.get_me(input_peer=True).user_id
-user = client.get_entity('ameet777')
-print(str(user.status))
+
+#Divyesh
+api_id = 308919
+api_hash = 'fff3e8b75bf9a6f147dda738eb2c640f'
+
+client = TelegramClient('session_name', api_id, api_hash)
+
+client.connect()
+
+
 
 def index(request):
     context = {}
-    context['group1'] = 'entrepreneurialjourney'
-    context['group2'] = 'joinexample3'
+    context['group1'] = 'Airdropbountycommunity'
+    context['group2'] = 'latestairdro'
+    if not client.is_user_authorized():
+        if request.method=="POST":
+            thephone = request.POST.get('group1')
+            client.send_code_request(thephone)
+            if request.POST.get('group2'):
+                thecode = request.POST.get('group2')
+                me = client.sign_in(thephone, thecode)
+                print("GOTIT")
+            else:
+                group1 = request.POST.get('group1')
+                return render(request, 'index.html', context)
+        else:
+            return render(request, 'index.html', context)
+    else:
+        me=client.get_me(input_peer=True).user_id
+
+    context['group1_client'] = client.get_participants(context['group1'])
+    context['group2_client'] = client.get_participants(context['group2'])
     if request.method == "POST":
         group1 = request.POST.get('group1')
         group2 = request.POST.get('group2')
-        context['group1_client'] = client.get_participants(context['group1'])
-        context['group2_client'] = client.get_participants(context['group2'])
-    '''
-    UserStatusOffline(was_online=datetime.datetime(2019, 1, 2, 9, 4, 8, tzinfo=datetime.timezone.utc))
-	UserStatusRecently()
-    '''
+
+        if request.POST["all"]:
+            context = {}
+            channels = {d.entity.username: d.entity
+                        for d in client.get_dialogs()
+                        if d.is_channel}
+
+            #channel='entrepreneurialjourney'
+            #channel2= 'joinexample3'
+            print("ALL PRESSED")
+            participants = client.get_participants(group1)
+            count=1
+            if str(1) == "1":
+                for u in participants:
+                    print(count)
+                    print(u.first_name)
+                    u_id=u.id
+                    last_seen = client.get_entity(u_id)
+                    print(str(last_seen))
+                    print()
+                    try:
+                        client(InviteToChannelRequest(group2,[u_id]))
+                    except:
+                        print("Privacy Issue")
+                    if count==50:
+                        context["group1"] = "It reached to 50 members addition"
+                        return render(request, 'index.html', context)
+                    count=count+1
+
     return render(request, 'index.html', context)
