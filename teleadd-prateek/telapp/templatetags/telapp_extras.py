ec164771714455e6b77fe41650a279852dcf99d7
dvskhamele <dvskha@gmail.com>
2019-01-03 13:02:08 +0530

    sc

--- teleadd-prateek/telapp/templatetags/telapp_extras.py
+++ teleadd-prateek/telapp/templatetags/telapp_extras.py
@@ -2,11 +2,10 @@ from django import template
 from django.utils.safestring import mark_safe
 register = template.Library()
 
-@register.filter(name='current_time')
-def current_time(status):
-    status = str(status)
-    if status=='UserStatusRecently()':
-        return mark_safe("<span class='badge badge-success'>online</span>")
-    else:
-        #sf = status.split('UserStatusOffline(was_online=datetime.datetime(')
-        return mark_safe("<span class='badge badge-dark'>ofline</span>")
+@register.filter(name='current_status')
+def current_status(status):
+    try:
+        status = status.was_online
+        return mark_safe("<span class='badge badge-dark'>offline since "+str(status)+"</span>")
+    except:
+    	return mark_safe("<span class='badge badge-success'>online</span>")
